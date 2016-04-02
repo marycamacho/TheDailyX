5b46b2fc2598786ea2a439c50c6187ce0f648e7b
Mary Camacho <maryfcamacho@gmail.com>
2016-04-01 20:03:17 -0600

    added function to class clicks to single, double and blank into squares

--- js/page.js
+++ js/page.js
@@ -3,6 +3,17 @@
  */
 
 $(function(){
-    //alert("hi");
+    $(".square").click(function () {
+        var this$=$(this);
+        if (this$.hasClass('single')){
+            this$.addClass('double').removeClass('single');
+        } else if (this$.hasClass('double')){
+            this$.removeClass('double');
+        } else {
+            this$.addClass('single');
+        }
+    })
+
+
 });
 
